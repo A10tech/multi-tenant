dc262716a9346c0be3e8a1c8e23e601d2551a249
Jeroen Boonstra <j.boonstra@a10tech.nl>
2017-07-05 12:01:59 +0200

    Implemented tenant option for migrate:status command.

--- src/Tenancy/Commands/Migrate/StatusCommand.php
+++ src/Tenancy/Commands/Migrate/StatusCommand.php
@@ -4,4 +4,43 @@ namespace Hyn\Tenancy\Commands\Migrate;
 
 class StatusCommand extends \Illuminate\Database\Console\Migrations\StatusCommand
 {
+    use TenantDatabaseCommandTrait;
+
+    /**
+     * @var \Hyn\Tenancy\Contracts\WebsiteRepositoryContract
+     */
+    protected $website;
+
+    /**
+     * MigrateCommand constructor.
+     *
+     * @param Migrator $migrator
+     */
+    public function __construct(Migrator $migrator)
+    {
+        parent::__construct($migrator);
+
+        $this->website = app('Hyn\Tenancy\Contracts\WebsiteRepositoryContract');
+    }
+
+    public function fire()
+    {
+        // fallback to default behaviour if we're not talking about multi tenancy
+        if (! $this->option('tenant')) {
+            $this->info('Not running tenancy migration, falling back on native laravel migrate command due to missing tenant option.');
+
+            //$this->input->setOption('database', config('multi-tenant.db.system-connection-name', 'hyn')); // use multi-tenant system database
+            return parent::fire();
+        }
+
+        $websites = $this->getWebsitesFromOption();
+
+        foreach ($websites as $website) {
+            $this->info("Migration status for {$website->id}: {$website->present()->name}");
+
+            $website->database->setCurrent();
+
+            parent::fire();
+        }
+    }
 }
