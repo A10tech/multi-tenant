7c3125d579947bebe470fadae328ba1f094bb259
Jeroen Boonstra <j.boonstra@a10tech.nl>
2017-07-05 16:25:52 +0200

    Extended SSL Certificate to store only filenames not certificate contents itself

--- src/Webserver/Models/SslCertificate.php
+++ src/Webserver/Models/SslCertificate.php
@@ -6,11 +6,22 @@ use Cache;
 use Config;
 use Hyn\Tenancy\Abstracts\Models\SystemModel;
 use Hyn\Tenancy\Models\Customer;
+use Hyn\Tenancy\Models\Hostname;
 use Hyn\Webserver\Tools\CertificateParser;
 use Laracasts\Presenter\PresentableTrait;
+use Illuminate\Support\Facades\File;
+use Carbon\Carbon;
 
 /**
  * Class SslCertificate.
+ *
+ * @property string pathCrt
+ * @property string pathKey
+ * @property string pathPem
+ * @property string pathCa
+ * @property Array pathCas
+ * @property Carbon validates_at
+ * @property Carbon invalidates_at
  */
 class SslCertificate extends SystemModel
 {
@@ -24,12 +35,12 @@ class SslCertificate extends SystemModel
     /**
      * @var array
      */
-    protected $fillable = ['tenant_id', 'certificate', 'authority_bundle', 'key'];
+    protected $fillable = ['customer_id', 'certificate', 'authority_bundle', 'key'];
 
     /**
      * @var array
      */
-    protected $appends = ['pathKey', 'pathPem', 'pathCrt', 'pathCa'];
+    protected $appends = ['pathKey', 'pathPem', 'pathCrt', 'pathCa', 'pathCas'];
 
     /**
      * @return array
@@ -50,7 +61,16 @@ class SslCertificate extends SystemModel
     public function getX509Attribute()
     {
         if (! Cache::has('ssl-x509-'.$this->id)) {
-            Cache::add('ssl-x509-'.$this->id, $this->certificate ? new CertificateParser($this->certificate) : null, 3600);
+            $sslCertificate = null;
+            if ($this->certificate) {
+                if (File::exists($this->certificate)) {
+                    $certificate = File::get($this->certificate);
+                    $sslCertificate = new CertificateParser($certificate);
+                } else {
+                    $sslCertificate = new CertificateParser($this->certificate);
+                }
+            }
+            Cache::add('ssl-x509-'.$this->id, $sslCertificate, 3600);
         }
 
         return Cache::get('ssl-x509-'.$this->id);
@@ -65,11 +85,23 @@ class SslCertificate extends SystemModel
     }
 
     /**
+     * Hostnames which uses this certificate
+     *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
     public function hostnames()
     {
-        return $this->hasMany(SslHostname::class);
+        return $this->hasMany(Hostname::class);
+    }
+
+    /**
+     * Hostnames defined in this certificate
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function certificateHostnames()
+    {
+        return $this->hasMany(sslHostname::class);
     }
 
     /**
@@ -77,13 +109,18 @@ class SslCertificate extends SystemModel
      */
     public function getPathKeyAttribute()
     {
-        return $this->publishPath('key');
+        $keyPath = $this->key;
+        if (File::exists($keyPath)) {
+            return $keyPath;
+        } else {
+            return $this->publishPath('key');
+        }
     }
 
     /**
      * @param string $postfix
      *
-*@return string
+     * @return string
      */
     public function publishPath($postfix = 'key')
     {
@@ -103,7 +140,12 @@ class SslCertificate extends SystemModel
      */
     public function getPathCrtAttribute()
     {
-        return $this->publishPath('crt');
+        $crtPath = $this->certificate;
+        if (File::exists($crtPath)) {
+            return $crtPath;
+        } else {
+            return $this->publishPath('crt');
+        }
     }
 
     /**
@@ -111,6 +153,84 @@ class SslCertificate extends SystemModel
      */
     public function getPathCaAttribute()
     {
-        return $this->publishPath('ca');
+        $caPaths = $this->getPathCasAttribute();
+        if (is_null($caPaths))
+            return $this->publishPath('ca');
+        if (empty($caPaths))
+            return "";
+        return $caPaths[0];
+    }
+    /**
+     *
+     * @return array|null
+     */
+    public function getPathCasAttribute()
+    {
+        if (empty($this->authority_bundle))
+            return []; // to distinguish valid empty, must return empty array and not null
+            $caPaths = explode('|', $this->authority_bundle);
+            foreach ($caPaths as $caPath) {
+                if (! File::exists($caPath))
+                    return null;
+            }
+            return $caPaths;
+    }
+
+    /**
+     *
+     * @param string $newFilePath
+     *
+     * @return void
+     */
+    public function setPathKeyAttribute($newFilePath)
+    {
+        if (File::exists($newFilePath)) {
+            $this->key = $newFilePath;
+        } else {
+            // error
+        }
+    }
+
+    /**
+     *
+     * @param string $newFilePath
+     *
+     * @return void
+     */
+    public function setPathCrtAttribute($newFilePath)
+    {
+        if (File::exists($newFilePath)) {
+            $this->certificate = $newFilePath;
+        } else {
+            // error
+        }
+    }
+
+    /**
+     *
+     * @param string $newFilePath
+     *
+     * @return void
+     */
+    public function setPathCaAttribute($newFilePath)
+    {
+        $this->setPathCasAttribute([
+            $newFilePath
+        ]);
+    }
+
+    /**
+     *
+     * @param array $newFilePaths
+     *
+     * @return void
+     */
+    public function setPathCasAttribute($newFilePaths)
+    {
+        foreach ($newFilePaths as $caPath) {
+            if (! File::exists($caPath))
+                return;
+        }
+        $this->authority_bundle = implode('|', $newFilePaths);
     }
 }
